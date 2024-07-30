3c35c441dc04e13570ffb4b780eb9167f6bc5c34
danielmlow <daniel.m.low@gmail.com>
2024-07-30 17:05:49 -0400

    fixed bug

--- src/construct_tracker/cts.py
+++ src/construct_tracker/cts.py
@@ -175,10 +175,7 @@ def measure(
 	# Embed construct 
 	# ================================================================================================
 	# Concatenate all tokens so we don't vectorize the same token multiple times
-	
-	
 	lexicon_tokens_concat = [item for sublist in lexicon_dict.values() for item in sublist]
-
 
 	if stored_embeddings_path is not None:
 		stored_embeddings = dill.load(open(stored_embeddings_path, "rb"))
