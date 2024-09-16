6de8926bc226d189e8cf00e23cd35aa89a48889f
Daniel M. Low <danielmlow@MacBook-Pro.local>
2024-09-15 20:11:56 -0400

    fixed load_datasets.py

--- src/construct_tracker/__init__.py
+++ src/construct_tracker/__init__.py
@@ -1,8 +1,7 @@
 """construct_tracker measures constructs in text."""
 
-from construct_tracker.data.datasets.load_datasets import load_data as _load_data
-
 from . import _version
+from .data.datasets.load_datasets import load_data as _load_data
 
 __version__ = _version.get_versions()["version"]
 __all__ = ["_load_data"]
